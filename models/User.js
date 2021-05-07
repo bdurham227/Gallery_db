a0fdc6fec31f485ddcc09b333fa139885e83870a
bdurham227 <bdurham227@gmail.com>
2021-05-07 12:51:02 -0400

    pushing completed models

--- models/User.js
+++ models/User.js
@@ -1,11 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DATE } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class User extends Model {}
 
 User.init(
     {
-
+        id: {
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type:DataTypes.STRING,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type:DataTypes.STRING,
+            validate: {
+                len: [6-100],
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true,
+        modelName: 'user',
     },
 );
 
