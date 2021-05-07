9fc9bc949d5ff88d1bcb611f04a31ecb0010326c
bdurham227 <bdurham227@gmail.com>
2021-05-07 12:54:41 -0400

    pushing associations of the models built on index.js up to github

--- models/index.js
+++ models/index.js
@@ -2,6 +2,13 @@ const Gallery = require('../models/Gallery');
 const Painting = require('../models/Painting');
 const User = require('../models/User');
 
+Gallery.hasMany(Painting, {
+    foreignKey: 'gallery_id',
+});
+Painting.belongsTo(Gallery, {
+    foreignKey: 'gallery_id',
+});
+
 
 
 
